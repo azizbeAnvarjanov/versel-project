176feba3fa73ae45ba8aa3007cbdee9f04178e71
azizbeAnvarjanov <https://github.com/azizbeAnvarjanov>
2022-08-18 09:05:35 +0500

    chabges sizes text

--- src/Components/BoshSahifa/BoshSahifa.js
+++ src/Components/BoshSahifa/BoshSahifa.js
@@ -203,7 +203,7 @@ function BoshSahifa() {
             transition={{ duration: 0.8 }}
             className="first"
           >
-            - {t("ko'p_beriladighan_savollar")} -
+            {t("ko'p_beriladighan_savollar")}
           </motion.h1>
           <div className="accordion-box">
             <motion.div
